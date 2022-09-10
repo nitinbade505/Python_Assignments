934a4a10c4acd81f7846c61c79879227b4243cca
nitinbade505 <101936013+nitinbade505@users.noreply.github.com>
2022-09-10 17:16:09 +0400

    assignment upload

--- Assignment 13/9.py
+++ Assignment 13/9.py
@@ -1,12 +1,6 @@
 #Write a Python script to create a list of city names taken from the user.
 
-print('enter a city name ')
-ll=list(input())
-print(ll)
-a,b,c,d,e,f,g=ll
-ll=[a+b+c,d+e+f+g]
-print(ll)
-
-print('enter a list of city ')
-ll=list(input())
+city1=input('enter a city name ')
+city2=input('enter a city name ')
+ll=[city1,city2]
 print(ll)
